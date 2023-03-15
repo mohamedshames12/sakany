4b763c8a4e48af41d5ac952dd4353e5e7a42975b
mohamed shames <mohamedhames900@gmail.com>
2023-03-15 14:11:02 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,19 @@
+const menu = document.getElementById("menu");
+const navBar = document.querySelector(".navber");
+
+menu.addEventListener("click", (eo) => {
+  navBar.classList.toggle("active")
+}
+)
+    
+window.onscroll = () => {
+    navBar.classList.remove("active");
+}
+
+const hacker = document.getElementById("hacker")
+
+
+hacker.addEventListener("click", (eo) => {
+    alert("Thank you for clicked on this button, I hecked your Phone .. شكرا علشان دوست ع الزرار انا كدا هكرت تلفونك باي باي ")
+}
+  )
