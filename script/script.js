8483993b527b1c6d75c0cc06326853a071412a39
mohamed shames <mohamedhames900@gmail.com>
2023-03-17 02:28:31 +0200

    update

--- script/script.js
+++ script/script.js
@@ -9,11 +9,3 @@ menu.addEventListener("click", (eo) => {
 window.onscroll = () => {
     navBar.classList.remove("active");
 }
-
-const hacker = document.getElementById("hacker")
-
-
-hacker.addEventListener("click", (eo) => {
-    alert("Thank you for clicked on this button, I hecked your Phone .. شكرا علشان دوست ع الزرار انا كدا هكرت تلفونك باي باي ");
-}
-  )
