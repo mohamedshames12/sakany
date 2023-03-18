2ac83eaebaee1ef5c508de365fdf7094b7778053
mohamed shames <mohamedhames900@gmail.com>
2023-03-18 19:41:37 +0200

    update

--- script/script.js
+++ script/script.js
@@ -9,3 +9,14 @@ menu.addEventListener("click", (eo) => {
 window.onscroll = () => {
     navBar.classList.remove("active");
 }
+
+
+let dropdown_items = document.querySelectorAll(".items");
+
+dropdown_items.forEach(items => {
+    items.onclick = () => {
+        items_parent = items.parentElement.parentElement;
+        const output = items_parent.querySelector(".output");
+        output.value = items.innerText;
+    }
+})
